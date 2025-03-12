a8f6b1fb4f4058fbcc0f88c3b82e01cffb46e814
Jinne Nijs <jinne.nijs@student.kuleuven.be>
2025-03-12 15:45:46 +0100

    Furhter implementation of Commands for mail managment

--- popserver.py
+++ popserver.py
@@ -1,15 +1,17 @@
 import socket
+import time
 from fileinput import close
 import pickle
+import sys
 
 
 def mailSearchingServer(socket, option):
     return
 
-
+#vraagt user en password in terminal
 def checkTextFile(user, password):
     with open("userinfo.txt") as file:
-        # read all lines of the file
+        # read all lines of the file for user and passwords
         lines = file.readlines()
         for line in lines:
             # if line starts with space, end of file
@@ -17,15 +19,18 @@ def checkTextFile(user, password):
                 break
             # separate user from password
             username = line.split()[0]
+
             if user == username:
                 actual_password = line.split()[1]
                 if password == actual_password:
                     file.close()
+                    #als alles klopt, return 1
                     return 1
     file.close()
+    #user /password niet in file  of fout
     return 0
 
-
+#vraagt username en password en kijkt of deze kloppen
 def userAuthentication(socket):
     while True:
         user_text = "USER"
@@ -34,15 +39,19 @@ def userAuthentication(socket):
         password_test = "PASS"
         socket.send(password_test.encode())
         received_password = socket.recv(1024).decode()
+        #checks if user and password are know and valid, returns 1 if true, 0 if not
         test = checkTextFile(received_user, received_password)
         if test == 1:
             connection_text = "+OK POP3 server is ready"
             socket.send(connection_text.encode())
+            #als user en password kloppen, return username
             return received_user
         else:
+            # anders try again
             error_text = "Wrong credentials, try again"
             socket.send(error_text.encode())
 
+#voor mail management: splitst mailbox om te sturen naar client
 def getMailInfo(mail):
     lines = mail.strip().split("\n")
     info = []
@@ -77,7 +86,8 @@ def findMails(username):
                     current_mail = []  # Reset for the next mail
 
                     ser_nr += 1  # Increment serial number for next email
-
+                else:
+                    break
             else:
                 # Collect the current mail's lines
                 current_mail.append(line)
@@ -85,17 +95,17 @@ def findMails(username):
             # Read the next line
             line = myfile.readline()
 
-        # After the loop, check if there is any remaining mail to process
-        if current_mail:
-            sender = current_mail[0]
-            subject = current_mail[2]
-            received = current_mail[3]
-            mailList.append([ser_nr, sender, received, subject])
-
     return mailList
+def performSTAT(maillist,username):
+    with open(username +"/my_mailbox","r") as myfile:
+        bytes = sys.getsizeof(myfile)
+    myfile.close()
+    length = findNumberOfMessages(maillist)
+    return length, bytes
 
-
-
+def findNumberOfMessages(list):
+    length = len(list)
+    return length
 def main():
     # specify which port to listen on
     my_port = 12346
@@ -131,10 +141,39 @@ def main():
         print(f"Received from client: {text}")
         # Mail management
         if text == "Mail Management" or text == "2":
+            #checkt username en password
             user = userAuthentication(c)
+            #verzamelt alle eerder gekregen mails van de user in een geordende lijst
             mailList = findMails(user)
             strList = str(mailList)
+            #geeft de mails terug aan de client
             c.send(strList.encode())
+            while True:
+                command = c.recv(1024).decode()
+                if command == "STAT":
+                    len,bytes = performSTAT(mailList,user)
+                    c.send((f"+OK {len} {bytes}").encode())
+                elif command.startswith("LIST"):
+
+                    if command == "LIST":
+                        print(mailList)
+                        len,bytes = performSTAT(mailList,user)
+                        c.send((f"+OK {len} {bytes}").encode())
+
+                        c.send(".".encode())
+                    else:
+                        command, nr = command.split()
+                        len, bytes = performSTAT(mailList[int(nr)],user)
+                        if nr > len:
+                            c.send(("ERROR nr is bigger than number of messages").encode())
+
+                        c.send((f"+OK {nr} {bytes}").encode())
+
+                elif command == "QUIT":
+                    # remove all messages markes as delete TO DO
+                    nmbrOfMess = str(findNumberOfMessages(mailList))
+                    c.send(("+OK pop3 server signing off (Number of messages left : "+ nmbrOfMess +")").encode())
+                    break
 
         # Mail Searching
         if text == "Mail Searching" or text == "3":
